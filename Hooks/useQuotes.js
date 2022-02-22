6ed6910e05f4f687ec9a512f997051bfe0ec6a9b
chrispydev <christianowusu44@gmail.com>
2022-02-22 13:39:15 +0000

    random quote

--- Hooks/useQuotes.js
+++ Hooks/useQuotes.js
@@ -3,11 +3,7 @@ import { quotes } from '../data/quotes';
 
 export default function useQuote() {
   const rIdx = Math.floor(Math.random() * quotes.length);
-  const [quote, setQuote] = useState(quotes[3]);
-
-  setInterval(() => {
-    setQuote(quotes[rIdx]);
-  }, 12000);
+  const [quote, setQuote] = useState(quotes[rIdx]);
 
   return quote;
 }
