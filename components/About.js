31d20c1f4c84e38e724f7f7c66f7f50bfcf933c1
chrispydev <christianowusu44@gmail.com>
2022-07-08 15:03:40 +0000

    changed <img /> from Image to image

--- components/About.js
+++ components/About.js
@@ -1,8 +1,8 @@
-import Image from "next/image";
-import { useState } from "react";
-import useProgressBar from "../Hooks/useProgressBar";
-import ProgressBar from "./ProgressBar";
-import aboutImage from "../public/images/about.jpg";
+import Image from 'next/image';
+import { useState } from 'react';
+import useProgressBar from '../Hooks/useProgressBar';
+import ProgressBar from './ProgressBar';
+import aboutImage from '../public/images/about.jpg';
 
 export default function About() {
   const python = useProgressBar(98);
@@ -27,25 +27,25 @@ export default function About() {
   const [show, setShow] = useState(false);
 
   return (
-    <section className="dark:bg-darker dark:text-white pt-16" id="about">
-      <div className="mx-5  lg:mx-10 flex flex-col lg:flex-row justify-between space-x-6">
-        <div className="w-full lg:w-1/4">
-          <Image
+    <section className='dark:bg-darker dark:text-white pt-16' id='about'>
+      <div className='mx-5  lg:mx-10 flex flex-col lg:flex-row justify-between space-x-6'>
+        <div className='w-full lg:w-1/4'>
+          {/* <Image
             src={aboutImage}
             alt="about"
             className="filter grayscale object-contain rounded-3xl"
-          />
-          {/* <img
+          /> */}
+          <img
             src='/images/about.jpg'
             alt='about'
             className='filter grayscale object-contain'
-          /> */}
+          />
         </div>
-        <div className="flex-1 pt-4 space-y-2">
-          <h1 className="text-bold capitalize text-3xl font-bold pb-2">
+        <div className='flex-1 pt-4 space-y-2'>
+          <h1 className='text-bold capitalize text-3xl font-bold pb-2'>
             About Me
           </h1>
-          <p className="text-gray-700 dark:text-gray-400 text-sm font-light leading-6 tracking-normal">
+          <p className='text-gray-700 dark:text-gray-400 text-sm font-light leading-6 tracking-normal'>
             My Name is Christian Owusu.I am an experienced Software and Web
             Developer adept in bringing forth expertise in design, installation,
             testing and maintenance of software systems. Equipped with a diverse
@@ -58,56 +58,56 @@ export default function About() {
             kind or work, I have got you covered.
           </p>
           <form
-            method="get"
-            action="/Christian_Owusu_-_Full_Stack_Developer.pdf"
+            method='get'
+            action='/Christian_Owusu_-_Full_Stack_Developer.pdf'
           >
-            <button className="py-2 px-2 rounded-full bg-secondary dark:bg-dark text-white">
+            <button className='py-2 px-2 rounded-full bg-secondary dark:bg-dark text-white'>
               Download resume
             </button>
           </form>
-          <h1 className="text-2xl font-bold">Skill Level</h1>
+          <h1 className='text-2xl font-bold'>Skill Level</h1>
 
-          <div className="mt-1 pb-8">
-            <div className="grid grid-cols-2 gap-8">
-              <ProgressBar percentage={python} name="python" />
-              <ProgressBar percentage={django} name="django" />
-              <ProgressBar percentage={react} name="react" />
-              <ProgressBar percentage={dart} name="dart" />
-              <ProgressBar percentage={reactNative} name="reactNative" />
-              <ProgressBar percentage={flutter} name="flutter" />
+          <div className='mt-1 pb-8'>
+            <div className='grid grid-cols-2 gap-8'>
+              <ProgressBar percentage={python} name='python' />
+              <ProgressBar percentage={django} name='django' />
+              <ProgressBar percentage={react} name='react' />
+              <ProgressBar percentage={dart} name='dart' />
+              <ProgressBar percentage={reactNative} name='reactNative' />
+              <ProgressBar percentage={flutter} name='flutter' />
               {show ? (
                 <>
-                  <ProgressBar percentage={javascript} name="javascript" />
-                  <ProgressBar percentage={jQuery} name="jQuery" />
-                  <ProgressBar percentage={html} name="html" />
-                  <ProgressBar percentage={css} name="css" />
-                  <ProgressBar percentage={less} name="less" />
-                  <ProgressBar percentage={sass} name="sass" />
-                  <ProgressBar percentage={materialUi} name="material-Ui" />
-                  <ProgressBar percentage={semanticUi} name="semantic-Ui" />
-                  <ProgressBar percentage={antd} name="antd" />
+                  <ProgressBar percentage={javascript} name='javascript' />
+                  <ProgressBar percentage={jQuery} name='jQuery' />
+                  <ProgressBar percentage={html} name='html' />
+                  <ProgressBar percentage={css} name='css' />
+                  <ProgressBar percentage={less} name='less' />
+                  <ProgressBar percentage={sass} name='sass' />
+                  <ProgressBar percentage={materialUi} name='material-Ui' />
+                  <ProgressBar percentage={semanticUi} name='semantic-Ui' />
+                  <ProgressBar percentage={antd} name='antd' />
                   <ProgressBar
                     percentage={materializeCss}
-                    name="materialize-css"
+                    name='materialize-css'
                   />
-                  <ProgressBar percentage={tailwindcss} name="tailwindcss" />
-                  <ProgressBar percentage={materialize} name="materialize" />
-                  <ProgressBar percentage={bootstrap} name="bootstrap" />
+                  <ProgressBar percentage={tailwindcss} name='tailwindcss' />
+                  <ProgressBar percentage={materialize} name='materialize' />
+                  <ProgressBar percentage={bootstrap} name='bootstrap' />
                 </>
               ) : null}
             </div>
           </div>
           <div
-            className="flex justify-center items-center space-x-1 cursor-pointer"
+            className='flex justify-center items-center space-x-1 cursor-pointer'
             onClick={() => setShow(!show)}
           >
             {show ? (
               <>
-                <p className="text-gray-700 font-bold uppercase">Hide</p>
+                <p className='text-gray-700 font-bold uppercase'>Hide</p>
               </>
             ) : (
               <>
-                <p className="text-gray-700 font-bold uppercase dark:text-white">
+                <p className='text-gray-700 font-bold uppercase dark:text-white'>
                   Show more
                 </p>
               </>
@@ -115,30 +115,30 @@ export default function About() {
             {show ? (
               <>
                 <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-4 w-4 text-gray-700 animate-bounce"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
+                  xmlns='http://www.w3.org/2000/svg'
+                  className='h-4 w-4 text-gray-700 animate-bounce'
+                  viewBox='0 0 20 20'
+                  fill='currentColor'
                 >
                   <path
-                    fillRule="evenodd"
-                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 0l-3 3a1 1 0 001.414 1.414L9 9.414V13a1 1 0 102 0V9.414l1.293 1.293a1 1 0 001.414-1.414z"
-                    clipRule="evenodd"
+                    fillRule='evenodd'
+                    d='M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 0l-3 3a1 1 0 001.414 1.414L9 9.414V13a1 1 0 102 0V9.414l1.293 1.293a1 1 0 001.414-1.414z'
+                    clipRule='evenodd'
                   />
                 </svg>
               </>
             ) : (
               <>
                 <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-4 w-4 text-gray-700 animate-bounce"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
+                  xmlns='http://www.w3.org/2000/svg'
+                  className='h-4 w-4 text-gray-700 animate-bounce'
+                  viewBox='0 0 20 20'
+                  fill='currentColor'
                 >
                   <path
-                    fillRule="evenodd"
-                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v3.586L7.707 9.293a1 1 0 00-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 10.586V7z"
-                    clipRule="evenodd"
+                    fillRule='evenodd'
+                    d='M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v3.586L7.707 9.293a1 1 0 00-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 10.586V7z'
+                    clipRule='evenodd'
                   />
                 </svg>
               </>
--- pages/uses.js
+++ pages/uses.js
@@ -1,31 +1,36 @@
-import Image from "next/image";
+import Image from 'next/image';
 
-import Heading from "../components/Heading";
-import HighlightText from "../components/HighlightText";
+import Heading from '../components/Heading';
+import HighlightText from '../components/HighlightText';
 
-import usesBanner from "../public/images/uses/banner.jpg";
+import usesBanner from '../public/images/uses/banner.jpg';
 
 export default function uses() {
   return (
-    <div className="dark:bg-darker">
-      <main className="lg:max-w-[80%] mx-auto p-2 dark:text-white">
-        <div className="p-4">
-          <Image
+    <div className='dark:bg-darker'>
+      <main className='lg:max-w-[80%] mx-auto p-2 dark:text-white'>
+        <div className='p-4'>
+          {/* <Image
             height={550}
             objectFit="cover"
             src={usesBanner}
             alt="user-banner"
+          /> */}
+          <img
+            src='/images/uses/banner.jpg'
+            alt='user-banner'
+            className='h-[200px] md:h-[550px] w-full object-cover'
           />
-          <div className="mt-7">
-            <div className="w-full flex justify-start items-center">
-              <Heading headingText="Uses" />
+          <div className='mt-7'>
+            <div className='w-full flex justify-start items-center'>
+              <Heading headingText='Uses' />
             </div>
-            <div className="font-lobsterTwo text-gray-700 flex justify-start items-center space-x-12 mt-3">
-              <p className="text-lg">December 12, 2019</p>
+            <div className='font-lobsterTwo text-gray-700 flex justify-start items-center space-x-12 mt-3'>
+              <p className='text-lg'>December 12, 2019</p>
               <p>Other</p>
             </div>
 
-            <ul className="uses-container">
+            <ul className='uses-container'>
               <li>
                 I have getting question about what I use for coding, apps I use
                 and so much.
@@ -36,20 +41,20 @@ export default function uses() {
                 mobile app or even web scrapping.
               </li>
               <li>
-                And if you have something you want to know just let me know{" "}
-                <a href="mailto:chrispydev.owusu@gmail.com">
-                  <HighlightText highlightText="chrispydev.owusu@gmail.com ." />
+                And if you have something you want to know just let me know{' '}
+                <a href='mailto:chrispydev.owusu@gmail.com'>
+                  <HighlightText highlightText='chrispydev.owusu@gmail.com .' />
                 </a>
               </li>
             </ul>
-            <div className="mt-8">
-              <div className="w-full flex justify-start items-center">
-                <Heading headingText="Machine & OS" />
+            <div className='mt-8'>
+              <div className='w-full flex justify-start items-center'>
+                <Heading headingText='Machine & OS' />
               </div>
-              <ul className="uses-container">
+              <ul className='uses-container'>
                 <li>
-                  My machine of choice is{" "}
-                  <HighlightText highlightText="Dell Latitude E7240 00 ." />{" "}
+                  My machine of choice is{' '}
+                  <HighlightText highlightText='Dell Latitude E7240 00 .' />{' '}
                   Which runs on a ubuntu Ubuntu 20.04.4 LTS x86_64 since I am a
                   linux user. I prefer linux or unix over windows because of
                   some past experience with windows.
@@ -57,21 +62,21 @@ export default function uses() {
                 <li>
                   I also started using garuda linux which is arch based linux
                   and awesome for coding and gaming.
-                  <a href="https://garudalinux.org/">
-                    <HighlightText highlightText="Garuda Linux  ." />{" "}
+                  <a href='https://garudalinux.org/'>
+                    <HighlightText highlightText='Garuda Linux  .' />{' '}
                   </a>
                   Plus you get any application you want to install with their
                   AUR and there is nothing like bloatware. Just like what you
                   get on other operating systems.
                 </li>
               </ul>
-              <div className="w-full flex justify-start items-center">
-                <Heading headingText="Desktop App" />
+              <div className='w-full flex justify-start items-center'>
+                <Heading headingText='Desktop App' />
               </div>
-              <ul className="uses-container">
+              <ul className='uses-container'>
                 <li>
-                  <a href="https://code.visualstudio.com/">
-                    <HighlightText highlightText="Vscode" /> is my day to day
+                  <a href='https://code.visualstudio.com/'>
+                    <HighlightText highlightText='Vscode' /> is my day to day
                     app
                   </a>
                   for coding. I do almost everything with vscode. With the
@@ -82,63 +87,63 @@ export default function uses() {
                 </li>
                 <li>
                   For terminal I
-                  <a href="https://hyper.is/">
-                    <HighlightText highlightText="HyperTerminal ." />
+                  <a href='https://hyper.is/'>
+                    <HighlightText highlightText='HyperTerminal .' />
                   </a>
                   Which is great and boost productivity. I get this immediately
                   when I install a new OS.
                 </li>
                 <li>
-                  <a href="https://github.com/alacritty/alacritty">
-                    <HighlightText highlightText="Alacrity ." />
+                  <a href='https://github.com/alacritty/alacritty'>
+                    <HighlightText highlightText='Alacrity .' />
                   </a>
                   The best terminal emulator for arch linux on my point of view.
                   It is a fast, cross-platform, OpenGL terminal emulator.
                 </li>
                 <li>
-                  <a href="https://developer.android.com/studio/intro">
-                    <HighlightText highlightText="Android Studio Code ." />
+                  <a href='https://developer.android.com/studio/intro'>
+                    <HighlightText highlightText='Android Studio Code .' />
                   </a>
                   As everyone know this is used for building app. I use this
                   mainly for flutter applications. I thank God that flutter is
                   so popular and that I can use it for church projects.
                 </li>
                 <li>
-                  I also use{" "}
-                  <a href="https://albertlauncher.github.io/installing/">
-                    <HighlightText highlightText="Albert Search" />
+                  I also use{' '}
+                  <a href='https://albertlauncher.github.io/installing/'>
+                    <HighlightText highlightText='Albert Search' />
                   </a>
                   for searching apps and on the web and sometimes use it for
                   calculations.
                 </li>
                 <li>
-                  My browser of choice is{" "}
-                  <a href="https://www.google.com/chrome/?brand=BNSD&gclid=Cj0KCQiAmeKQBhDvARIsAHJ7mF4Gde837R05zmnze00ChHTZ9gKEkYSP3SPhPc0Igt-hWueYVAqYET0aAnaZEALw_wcB&gclsrc=aw.ds">
-                    <HighlightText highlightText=" google chrome" />
-                  </a>{" "}
-                  and{" "}
-                  <a href="https://www.mozilla.org/en-US/firefox/new/">
-                    <HighlightText highlightText="firefox ." />
+                  My browser of choice is{' '}
+                  <a href='https://www.google.com/chrome/?brand=BNSD&gclid=Cj0KCQiAmeKQBhDvARIsAHJ7mF4Gde837R05zmnze00ChHTZ9gKEkYSP3SPhPc0Igt-hWueYVAqYET0aAnaZEALw_wcB&gclsrc=aw.ds'>
+                    <HighlightText highlightText=' google chrome' />
+                  </a>{' '}
+                  and{' '}
+                  <a href='https://www.mozilla.org/en-US/firefox/new/'>
+                    <HighlightText highlightText='firefox .' />
                   </a>
                   They all have their own props👍 and cons👎 in terms of
                   developments. One can do things better than other so I keep
                   these two all the time.
                 </li>
                 <li>
-                  <a href="https://discord.com/">
-                    <HighlightText highlightText="Discord" />
-                  </a>{" "}
+                  <a href='https://discord.com/'>
+                    <HighlightText highlightText='Discord' />
+                  </a>{' '}
                   app is my way of keeping up to date with the community I am
                   connected to.
                 </li>
               </ul>
-              <div className="w-full flex justify-start items-center">
-                <Heading headingText="Services for Hosting" />
+              <div className='w-full flex justify-start items-center'>
+                <Heading headingText='Services for Hosting' />
               </div>
-              <ul className="uses-container">
+              <ul className='uses-container'>
                 <li>
-                  <a href="https://docs.github.com/en/pages/quickstart">
-                    <HighlightText highlightText="Github Pages" />
+                  <a href='https://docs.github.com/en/pages/quickstart'>
+                    <HighlightText highlightText='Github Pages' />
                   </a>
                   Github pages is free and allows everyone to host their static
                   websites for free and even add a domain name as well. And I
@@ -146,8 +151,8 @@ export default function uses() {
                   vercel and netlify.
                 </li>
                 <li>
-                  <a href="https://www.netlify.com/">
-                    <HighlightText highlightText="Netlify" />
+                  <a href='https://www.netlify.com/'>
+                    <HighlightText highlightText='Netlify' />
                   </a>
                   is very popular when it comes to web hosting and free services
                   up to some point. Even though some know them as static web
@@ -158,8 +163,8 @@ export default function uses() {
                 {/* correct the spell of favorite */}
                 <li>
                   My favorite web hosting services now is
-                  <a href="https://vercel.com/">
-                    <HighlightText highlightText="Vercel" />
+                  <a href='https://vercel.com/'>
+                    <HighlightText highlightText='Vercel' />
                   </a>
                   and they are already configured for nextjs and its easy to use
                   with easy user interface.
